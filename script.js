018cc9cd52c81f3ce5058fc738faf488c6dbb192
gustavolessagoncalves2 <86855871+gustavolessagoncalves2@users.noreply.github.com>
2022-03-28 09:59:48 -0300

    finalizado HTML e CSS

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//Selecionar -> Botão Rate clicado fica cinza claro, todos os outros botões Rate ficam cinza escuro. Adiciona o valor do botão clicado numa variável.
+
+//Submeter -> Muda de página. Adiciona o valor na mensagem na página.
+
+
 function submit() {
 
 }
+
