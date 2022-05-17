dac9854e4eef96a1717e278f7cdd3bb66f751fc7
gustavolessagoncalves2 <86855871+gustavolessagoncalves2@users.noreply.github.com>
2022-05-17 17:28:08 -0300

    implementando solução
    modifiquei o javascript, inserindo nova lógica de selecionar os botões

--- script.js
+++ script.js
@@ -1,25 +1,73 @@
-//Select -> Botão Rate clicado fica cinza claro, todos os outros botões Rate ficam cinza escuro. Adiciona o valor do botão clicado numa variável. 
+//Select -> Botão Rate clicado fica cinza claro, todos os outros botões Rate ficam cinza escuro. Adiciona o valor do botão clicado numa variável. Alterna a Class entre select e rate
 
-//Adicionar ouvidores nos botões
 
 //Melhoria: USAR QUERYSELECTALL E FOR
 
 //-------------------------------------------------------
     
+//PASSO 1 - Pegar os valores dos buttons
 
-var buttons = document.querySelectorAll('button[type="button"]')
+const txtButton1 = document.querySelector('button[value="1"]')
+const Button1 = txtButton1.value
 
-for(var i=0; i<buttons.length; i++) {
-    buttons[i].addEventListener("click", function() {
-        this.style.backgroundColor = "red";
-    })
+const txtButton2 = document.querySelector('button[value="2"]')
+const Button2 = txtButton2.value
+
+const txtButton3 = document.querySelector('button[value="3"]')
+const Button3 = txtButton3.value
+
+const txtButton4 = document.querySelector('button[value="4"]')
+const Button4 = txtButton4.value
+
+const txtButton5 = document.querySelector('button[value="5"]')
+const Button5 = txtButton5.value
+
+const txtSubmitButton = document.querySelector('button.submit')
+
+//PASSO 2 - Mudar a cor do botão clicado (aplicar a class select no button clicado e aplicar a class select nos demais buttons) e habilitar o button subtmit
+
+function select1() {
+    txtButton1.classList.toggle("select") 
+    txtButton2.classList.remove("select") 
+    txtButton3.classList.remove("select") 
+    txtButton4.classList.remove("select") 
+    txtButton5.classList.remove("select")
+    txtSubmitButton.removeAttribute('disabled')
+}
+function select2() {
+    txtButton1.classList.remove("select") 
+    txtButton2.classList.toggle("select") 
+    txtButton3.classList.remove("select") 
+    txtButton4.classList.remove("select") 
+    txtButton5.classList.remove("select") 
+    txtSubmitButton.removeAttribute('disabled')
+}
+function select3() {
+    txtButton1.classList.remove("select") 
+    txtButton2.classList.remove("select") 
+    txtButton3.classList.toggle("select") 
+    txtButton4.classList.remove("select") 
+    txtButton5.classList.remove("select") 
+    txtSubmitButton.removeAttribute('disabled')
+}
+function select4() {
+    txtButton1.classList.remove("select") 
+    txtButton2.classList.remove("select") 
+    txtButton3.classList.remove("select") 
+    txtButton4.classList.toggle("select") 
+    txtButton5.classList.remove("select") 
+    txtSubmitButton.removeAttribute('disabled')
+}
+function select5() {
+    txtButton1.classList.remove("select") 
+    txtButton2.classList.remove("select") 
+    txtButton3.classList.remove("select") 
+    txtButton4.classList.remove("select") 
+    txtButton5.classList.toggle("select") 
+    txtSubmitButton.removeAttribute('disabled')
 }
 
-
-
-
-
-
+//PASSO 3 - Pegar somente o Button com a class select
 
 
 
